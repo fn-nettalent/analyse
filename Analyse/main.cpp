4641d05301dddc3858bc79afc2ad36a7809e1104
cagatay arslan <ca@cas-mbp.lan>
2017-07-26 20:09:50 +0300

    Initial Commit

---
+++ Analyse/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Analyse
+//
+//  Created by cagatay arslan on 26/07/2017.
+//  Copyright © 2017 cagatay arslan. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
